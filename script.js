7139f830e59472551935b577f3d65851ce50f431
Xingtitan <dat.tran@vizion.space>
2022-07-27 17:33:48 +0700

    add mobile interface

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-const hamburgerIcon = document.querySelector('.menu-btn');
-const overlay = document.querySelector('.overlay');
-const mobileNav = document.querySelector('.mobile-nav');
+const hamburgerIcon = document.querySelector(".menu-btn");
+const overlay = document.querySelector(".overlay");
+const mobileNav = document.querySelector(".mobile-nav");
+const body = document.getElementsByTagName("body")[0];
 
-hamburgerIcon.addEventListener('click', isToggledMenu);
+hamburgerIcon.addEventListener("click", isToggledMenu);
 
 function isToggledMenu() {
-    hamburgerIcon.classList.toggle("isToggled");
-    overlay.classList.toggle("isToggled");
-    mobileNav.classList.toggle("isToggled");
-};
+  hamburgerIcon.classList.toggle("isToggled");
+  overlay.classList.toggle("isToggled");
+  mobileNav.classList.toggle("isToggled");
+  body.classList.toggle("isToggled");
+}
